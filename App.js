3f814e7147ebe5114c2d55c5c0309a72b8959167
Bobby Schade <bschade.18@gmail.com>
2021-11-09 19:31:37 -0600

    fetch hunt data and add api client

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { client } from './utils/api-client';
 
 export default function App() {
+  const [status, setStatus] = React.useState('idle');
+  const [data, setData] = useState();
+  const [error, setError] = React.useState();
+
+  const isLoading = status === 'loading';
+  const isSuccess = status === 'success';
+  const isError = status === 'error';
+
+  useEffect(() => {
+    client(
+      'https://www.letsroam.com/coding-challenge-endpoint?password=81j2jj210a9'
+    ).then(
+      (responseData) => {
+        setData(responseData);
+        setStatus('success');
+      },
+      (errorData) => {
+        setError(errorData);
+        setStatus('error');
+      }
+    );
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Let's Roam!</Text>
+      {isError && <Text>There was an error</Text>}
+      {isSuccess && <Text>Let's Roam!!!!</Text>}
       <StatusBar style="auto" />
     </View>
   );
